f1b135f4547860ecec3ee96b7f2a5095603a4715
Alejandro Barazarte <abx.com@gmail.com>
2015-12-30 17:31:17 -0430

    added routes and user seed

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -29,7 +29,9 @@ Route::get('/', function () {
 Route::group(['middleware' => ['web']], function () {
     //
 });
-Route::group(array('prefix' => 'api/v1'), function()
-    {
-        Route::resource('users', 'UserController');
+Route::group(array('prefix' => 'api/v1'), function(){
+        Route::resource('payment','PaymentController',[ 'only'=>['index','show'] ]);
+        Route::resource('users', 'UserController', ['except'=>['create','edit']]);
+        Route::resource('users.favorites','UserFavoritesController', ['except'=>['create','edit', 'show', 'update']]);
+        Route::resource('users.payments','UserPaymentsController', ['except'=>['create','edit', 'show', 'update']]);
     });
